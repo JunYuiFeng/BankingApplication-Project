fa82eb8a963d9d0ed5a234e660bbc7538f1be005
junyuifeng <678234@student.inholland.nl>
2023-04-21 00:12:11 +0200

    Java springboot file added

---
+++ src/main/java/nl/inholland/bankingapplication/BankingapplicationApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.bankingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/inholland/bankingapplication/BankingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.bankingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
