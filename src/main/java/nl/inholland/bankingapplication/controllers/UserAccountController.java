6cc57a68a66f653e183bf4a3f806a0d2fece4022
Alessandra Ribeiro <689693@student.inholland.nl>
2023-05-11 21:23:45 +0200

    added the CorsConfig file
    added the CorsConfig file in order to be able to make the requests from the front end

--- src/main/java/nl/inholland/bankingapplication/controllers/UserAccountController.java
+++ src/main/java/nl/inholland/bankingapplication/controllers/UserAccountController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
+
 @RequestMapping("UserAccounts")
 public class UserAccountController {
     private UserAccountService userAccountService;
