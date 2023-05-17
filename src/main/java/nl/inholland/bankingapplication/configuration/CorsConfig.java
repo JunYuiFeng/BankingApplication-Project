d1d4549c3aa16de3cff39d867aa4c890b0169ebe
Alessandra Ribeiro <689693@student.inholland.nl>
2023-05-17 18:22:09 +0200

    added corsConf file
    added corsConf file

--- src/main/java/nl/inholland/bankingapplication/configuration/CorsConfig.java
+++ src/main/java/nl/inholland/bankingapplication/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5175")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
