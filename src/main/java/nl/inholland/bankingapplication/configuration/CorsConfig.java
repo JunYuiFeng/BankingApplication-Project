3fc39e4835e3067bc21d85fdf0f271bb077a8134
Jason Xie <87727651+JasonXie62@users.noreply.github.com>
2023-05-20 22:52:31 +0200

    small fix

--- src/main/java/nl/inholland/bankingapplication/configuration/CorsConfig.java
+++ src/main/java/nl/inholland/bankingapplication/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
