626b614d969e0da2264bfd5a8abc8d237da7d124
Alessandra Ribeiro <689693@student.inholland.nl>
2023-05-21 11:18:58 +0200

    implemented the inholland.p12 class, implemented https, maxsize  filter, added key helper, JWT key provider
    implemented the inholland.p12 class, implemented https, maxsize  filter, added key helper, JWT key provider

---
+++ src/main/java/nl/inholland/bankingapplication/util/JWTKeyProvider.java
@@ -0,0 +1,40 @@
+package nl.inholland.bankingapplication.util;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+public class JWTKeyProvider {
+    @Value("${jwt.key-store}")
+    private String keystore;
+
+    @Value("${jwt.key-alias}")
+    private String keyAlias;
+
+    @Value("${jwt.key-store-password}")
+    private String keyStorePassword;
+
+    private Key privateKey;
+
+    public Key getPrivateKey() {
+
+        return privateKey;
+    }
+    @PostConstruct //makes sure that this class is made after everything is initialized
+    protected void init() throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, IOException, CertificateException {
+        Resource resource = new ClassPathResource(keystore);
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(resource.getInputStream(), keyStorePassword.toCharArray());
+        privateKey = keyStore.getKey(keyAlias, keyStorePassword.toCharArray());
+    }
+
+}
---
+++ src/main/java/nl/inholland/bankingapplication/util/KeyHelper.java
@@ -0,0 +1,33 @@
+package nl.inholland.bankingapplication.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+public class KeyHelper {
+    private KeyHelper() {
+
+    }
+    @Value("${jwt.key-store-type}")
+    private  static String keyStoreType;
+
+
+    public static Key generateKey(String alias, String keystore, String password) throws KeyStoreException, IOException, UnrecoverableKeyException, NoSuchAlgorithmException, CertificateException {
+        //checks  if the credentials are correct (inside the inholland.p12 files) before the JWT is generated to see if the values matches
+        Resource resource = new ClassPathResource(keystore);
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(resource.getInputStream(), password.toCharArray());
+        return keyStore.getKey(alias, password.toCharArray()); }
+
+
+
+
+}
