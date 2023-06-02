ef90e01669245c349d52e27b196c231ea85473ad
Alessandra Ribeiro <689693@student.inholland.nl>
2023-06-02 11:29:35 +0200

    @crosOrigin
    @crosOrigin

--- src/main/java/nl/inholland/bankingapplication/controllers/LoginController.java
+++ src/main/java/nl/inholland/bankingapplication/controllers/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 
 @RestController
-
+@CrossOrigin
 @RequestMapping("/login")
 public class LoginController {
     private UserAccountService userService;
